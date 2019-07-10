1126c84dbc1ca4b918906989fe421140075cf108
Pedro Peixoto <ppeixoto@usp.br>
2019-07-09 21:52:57 -0300

    mobile dynamics simulation

--- mdyn_domain.py
+++ mdyn_domain.py
@@ -27,20 +27,22 @@ class Domain:
         self.nlon = int((self.maxlons - (self.minlons))/self.dlon)
         self.nlat = int((self.maxlats - (self.minlats))/self.dlat)
 
-        self.lon_bins = np.linspace(self.minlons, self.maxlons, self.nlon+1, endpoint=True) 
-        self.lat_bins = np.linspace(self.minlats, self.maxlats, self.nlat+1, endpoint=True) 
-        #print(self.lon_bins)
-        self.lon_bins_c = np.linspace(self.minlons-self.dlon/2, self.maxlons+self.dlon/2, self.nlon+2, endpoint=True) 
-        self.lat_bins_c = np.linspace(self.minlats-self.dlat/2, self.maxlats+self.dlat/2, self.nlat+2, endpoint=True) 
-        #print(self.lon_bins_c)
-        self.lon_bins_2d, self.lat_bins_2d = np.meshgrid(self.lon_bins, self.lat_bins)
-        self.lon_bins_2d_c, self.lat_bins_2d_c = np.meshgrid(self.lon_bins_c, self.lat_bins_c)
+        #These are the datapoints
+        self.lon_bins_c = np.linspace(self.minlons, self.maxlons, self.nlon+1, endpoint=True) 
+        self.lat_bins_c = np.linspace(self.minlats, self.maxlats, self.nlat+1, endpoint=True) 
+
+        #These are bins for datagrid (larger than datagrid) - Extended grid
+        self.lon_bins_ext = np.linspace(self.minlons-self.dlon/2, self.maxlons+self.dlon/2, self.nlon+2, endpoint=True) 
+        self.lat_bins_ext = np.linspace(self.minlats-self.dlat/2, self.maxlats+self.dlat/2, self.nlat+2, endpoint=True) 
+
+        #2d grids
+        self.lon_bins_c_2d, self.lat_bins_c_2d = np.meshgrid(self.lon_bins_c, self.lat_bins_c)
+        self.lon_bins_ext_2d, self.lat_bins_ext_2d = np.meshgrid(self.lon_bins_ext, self.lat_bins_ext)
 
 
     def set_global_domain(self, data):
 
         #data needs to have days with precalculated domains
-
         self.minlons=100000.0
         self.maxlons=-1000000.0
         self.minlats=100000.0
@@ -86,6 +88,7 @@ class Map:
         #    resolution='h',area_thresh=100.,projection='lcc',\
         #    lat_1=-20.4,lat_2=-24.2,\
         #    lat_0=-22,lon_0=-48.3)
+
         #Config map
         map.drawcoastlines()
         map.drawcountries()
@@ -96,8 +99,8 @@ class Map:
         map.ax = ax
         
         # convert the bin mesh to map coordinates:
-        self.x_bins, self.y_bins = map(dom.lon_bins_2d, dom.lat_bins_2d) # will be plotted using pcolormesh
-        self.x_bins_c, self.y_bins_c = map(dom.lon_bins_2d_c, dom.lat_bins_2d_c) # will be plotted using pcolormesh
+        self.x_bins_c, self.y_bins_c = map(dom.lon_bins_c_2d, dom.lat_bins_c_2d) # will be plotted using pcolormesh
+        self.x_bins_ext, self.y_bins_ext = map(dom.lon_bins_ext_2d, dom.lat_bins_ext_2d) # will be plotted using pcolormesh
 
         #Save fig basic config
         self.map = map
@@ -107,10 +110,10 @@ class Map:
     def map_density_data(self, lng, lat, title, dir):
         
         #Calculate 2d histogram
-        density, _, _ = np.histogram2d(lat, lng, [self.dom.lat_bins_c, self.dom.lon_bins_c])
+        density, _, _ = np.histogram2d(lat, lng, [self.dom.lat_bins_ext, self.dom.lon_bins_ext], density=False)
 
         #Create color plot
-        plt.pcolormesh(self.x_bins_c, self.y_bins_c, density, cmap="hot_r", norm=colors.LogNorm(), snap=True)
+        plt.pcolormesh(self.x_bins_ext, self.y_bins_ext, density, cmap="hot_r", norm=colors.LogNorm(), snap=True)
 
         #Add stuf to plot
         cbar = plt.colorbar(orientation='horizontal', shrink=0.5, aspect=20, fraction=0.1, pad=0.01)
@@ -124,7 +127,7 @@ class Map:
     def map_data(self, data, title, dir):
         
         #2d color plot of data
-        plt.pcolormesh(self.x_bins_c, self.y_bins_c, data)  
+        plt.pcolormesh(self.x_bins_ext, self.y_bins_ext, data, density=False)  
             #cmap="hot_r", norm=colors.LogNorm(), snap=True)
 
         #Config
@@ -137,30 +140,35 @@ class Map:
 
     def map_reg_data(self, network, title, dir):
         
-        data = network.region_grid
+        data=network.region_grid
+        data=data.astype(int)
 
-        reg = np.unique(data)
-        print(reg)
-        #cols = colors.cnames.keys()
-        cols = ['blue', 'red', 'yellow', 'orange',  'limegreen', 'violet', 'purple', 'brown', 'cyan', 'olive', 'coral','lightgreen' ,'grey']
+        #np.savetxt('tmp.txt', data, fmt='%3i')
+        #reg = np.unique(data)
         
-        cmap = colors.ListedColormap(cols, N=len(cols))
+        #cols = colors.cnames.keys()
+        cols = ['blue', 'red', 'yellow', 'orange',  'limegreen', \
+            'violet', 'purple', 'brown', 'cyan', 'olive', 'coral','lightgreen' ,'grey']
+        
+        cmap = colors.ListedColormap(cols, N=(network.nregions+1))
         #boundaries = reg 
         #norm = colors.BoundaryNorm(boundaries, len(cols), clip=True)
         
         #2d color plot of data
-        plt.pcolormesh(self.x_bins_c, self.y_bins_c, data, cmap=cmap) #, norm=norm)  
+        plt.pcolormesh(self.x_bins_ext, self.y_bins_ext, data, cmap=cmap, snap=True) #, norm=norm)  
             #cmap="hot_r", norm=colors.LogNorm(), snap=True)
 
         #Config
         cbar = plt.colorbar(orientation='horizontal', shrink=0.5, aspect=20, fraction=0.1, pad=0.01)
         cbar.set_label(title,size=12)
+        cbar.set_ticks(range(-1, network.nregions+1, 1))
+        cbar.set_ticklabels(range(-1, network.nregions+1, 1))
     
         #Add labels 
         reg = list(network.regions_in_latlon.values())
         
         for lat, lon, i in reg:
-            print(lon, lat, i)
+            #print(lon, lat, i)
             xpt, ypt = self.map([lon], [lat])
             try: 
                 #print(xpt[0], ypt[0], str(network.region_full.get(i)))
--- mdyn_run.py
+++ mdyn_run.py
@@ -5,14 +5,15 @@
 import sys
 import os
 import warnings
+warnings.filterwarnings("ignore")
 
 from mdyn_main import MobileDynamics
-
-warnings.filterwarnings("ignore")
 
 #Initialize mobile data and load data to dataframe 
 mdyn=MobileDynamics(sys.argv)
 
 #Calculate analytics
 #mdyn.build_analytics_data(mode="all", state="SP")
-mdyn.build_analytics_data(mode="reg", state="SP")
+mdyn.build_model(mode="reg", state="SP")
+
+mdyn.simulate(mode="reg")
