b984a92ab17710e9f1837d542a0708228e62bc83
Pedro Peixoto <ppeixoto@usp.br>
2020-03-27 11:21:13 -0300

    mdyn_build_model.py

---
+++ mdyn_build_model.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# conda enviroment
+# conda activate mdyn
+
+import sys
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+from mdyn_main import MobileDynamics
+
+#Initialize mobile data and load data to dataframe 
+mdyn=MobileDynamics(sys.argv)
+
+#Calculate analytics
+#mdyn.build_analytics_data(mode="all", state="SP")
+state = "SP"
+mdyn.build_model(mode="reg", state="SP", precompdomain = True)
+
