8cf02e655dcb77d74bab965b7a5fd8d6e0885b67
Pedro Peixoto <ppeixoto@usp.br>
2020-04-01 08:50:45 -0300

    move mat updates

--- mdyn_move_mats.py
+++ mdyn_move_mats.py
@@ -13,29 +13,8 @@ def analyse_move_mats(mdyn, network, ipar):
     #Analyse movement matrices
     mdyn.collect_move_mat(network.domain, network.subdomains)
 
-    #Period mean move mat
-    movemat_avg = np.zeros(mdyn.movemats[0].shape)
-
-    #Loop work with transitions matrices and average then
-    for i, day in enumerate(mdyn.days_all):
-        print("Load: ", i, day)
-        
-        title_base = "move_mat_"+network.domain+"_"+network.subdomains+"_"+day.strftime("%Y-%m-%d")
-        mex.plot_matrix(mdyn.movemats_norm[i], title_base+"\nDay_Prob_Move", mdyn.dump_dir)
-        movemat_avg = movemat_avg + mdyn.movemats[i]
-        #mex.matprint(mdyn.movemats_norm[i])
-
-    #Get primary and secondary sources
-    movemat_avg_diag = np.diag(movemat_avg)
-    movemat_avg_diag = movemat_avg_diag[0:network.nreg_in]
-    #print(movemat_avg_diag)
-    movemat_avg = movemat_avg / movemat_avg.sum(axis=0)
-
-    title_base = "move_mat_"+network.domain+"_"+network.subdomains+"_"+mdyn.date_ini+"_"+mdyn.date_end
-    mex.plot_matrix(movemat_avg, title_base+"\nMean_Prob", mdyn.dump_dir)
-
-    movemat_std = np.std(mdyn.movemats_norm, axis=0)
-    mex.plot_matrix(movemat_std, title_base+"\nStd_Dev_of_Prob", mdyn.dump_dir)
+    #Calculate matrix statistics
+    movemat_avg, movemat_std, movemat_avg_diag = calc_move_mat_avg(mdyn, network, ipar)
 
     #Sources to plot
     num_source = ipar.num_source #min(network.nregions-2, 20)
@@ -48,6 +27,7 @@ def analyse_move_mats(mdyn, network, ipar):
     #Join all main sources
     sources = np.unique(np.concatenate((sources, sources2), axis=0) )
 
+    title_base = "move_mat_"+network.domain+"_"+network.subdomains+"_"+mdyn.date_ini+"_"+mdyn.date_end
 
     print("Regions:", network.regions)
     print("Main regions:", sources)
@@ -69,6 +49,39 @@ def simulate_move_mats(mdyn, network, ipar):
     #Analyse movement matrices
     mdyn.collect_move_mat(network.domain, network.subdomains)
 
+    movemat_avg, movemat_std, movemat_avg_diag = calc_move_mat_avg(mdyn, network, ipar)
+    #Sources to plot 
+    #num_simul_days = ipar.num_simul_days #min(network.nregions-2, 20)
+
+    #Initial condition
+    data_ini_regv = np.zeros([network.nregions])
+    data_ini_regv[12] = 100.0
+    day_state = data_ini_regv
+
+    title_base = "move_mat_"+network.domain+"_"+network.subdomains+"_"+mdyn.date_ini+"_"+mdyn.date_end
+    #simulate scenario
+    for i, day in enumerate(mdyn.days_all):
+    #for j in num_simul_days:
+        
+        title = title_base+"_day_"+day.strftime("%Y-%m-%d")
+        print("Creating plot for ", title)
+            
+        map=Map(network)
+        map.map_move_by_reg(day_state, network.regions, network, title, mdyn.dump_dir+title)
+
+        if ipar.simul_type == "avg":
+            mat = movemat_avg
+        else:
+            mat = mdyn.movemats_norm[i]
+            day_state=np.matmul(mat, day_state)
+        sumv = np.sum(day_state)
+
+        if abs(sumv-np.sum(data_ini_regv))>0.001:
+            print("Warning: Mass not being conserved! ", sumv)
+    
+
+
+def calc_move_mat_avg(mdyn, networ, ipar):
     #Period mean move mat
     movemat_avg = np.zeros(mdyn.movemats[0].shape)
 
@@ -100,36 +113,10 @@ def simulate_move_mats(mdyn, network, ipar):
         movemat_std = np.std(mdyn.movemats_norm, axis=0)
         mex.plot_matrix(movemat_std, title_base+"\nStd_Dev_of_Prob", filename)
 
-    #Sources to plot 
-    #num_simul_days = ipar.num_simul_days #min(network.nregions-2, 20)
+    return movemat_avg,  movemat_std, movemat_avg_diag
 
-    #Initial condition
-    data_ini_regv = np.zeros([network.nregions])
-    data_ini_regv[12] = 100.0
-    day_state = data_ini_regv
 
-    #simulate scenario
-    for i, day in enumerate(mdyn.days_all):
-    #for j in num_simul_days:
-        
-        title = title_base+"_day_"+day.strftime("%Y-%m-%d")
-        print("Creating plot for ", title)
-            
-        map=Map(network)
-        map.map_move_by_reg(day_state, network.regions, network, title, mdyn.dump_dir+title)
-
-        if ipar.simul_type == "avg":
-            mat = movemat_avg
-        else:
-            mat = mdyn.movemats_norm[i]
-            day_state=np.matmul(mat, day_state)
-        sumv = np.sum(day_state)
-        
-        if abs(sumv-np.sum(data_ini_regv))>0.001:
-            print("Warning: Mass not being conserved! ", sumv)
-    
-
-    def simulate_daily(self, mode):
+"""     def simulate_daily(self, mode):
     
         #initial condition
         
@@ -214,5 +201,5 @@ def simulate_move_mats(mdyn, network, ipar):
             del ax.lines[1]   
         
         
-
+ """
         
