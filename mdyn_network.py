5cccb12b6cc361565f6b7f4d4c6d6d9dc4430418
Diego Marcondes <diegormmarcondes@gmail.com>
2020-04-03 09:44:08 -0300

    Deleted loaf_pop call for test

--- mdyn_network.py
+++ mdyn_network.py
@@ -22,7 +22,7 @@ import geopandas as gpd
 from shapely.geometry import Point, Polygon
 
 
-import tqdm 
+import tqdm
 
 from mdyn_daydata import DayData
 from mdyn_map import Map
@@ -33,14 +33,14 @@ class Network:
 
     #cities = {}
 
-    def __init__(self, 
-        domain = None, 
+    def __init__(self,
+        domain = None,
         domain_gran = None,
-        domain_shape = None, 
+        domain_shape = None,
         domain_pop=None,
         domain_pop_labels=None,
         subdomains = None,
-        subdomains_gran = None, 
+        subdomains_gran = None,
         subdomains_shape=None,
         subdomains_pop=None,
         subdomains_pop_labels=None,
@@ -65,7 +65,7 @@ class Network:
         self.load = load
 
         #Load main domain
-        self.df_domain = self.load_domain() 
+        self.df_domain = self.load_domain()
 
         #Load subdomains
         self.df_subdomains = self.load_subdomains()
@@ -77,7 +77,7 @@ class Network:
         self.build_grid_network()
 
         #Load regions' populations
-        self.load_pop()
+        #self.load_pop()
 
     def load_domain(self):
 
@@ -94,27 +94,27 @@ class Network:
         else: #Build map structure with neighbours
             df = gpd.read_file(self.domain_shape_file)
             print("  Creating basic domain shape file...", end = '')
-            
+
             df["NEIGHBORS"] = None  # add NEIGHBORS column
-            for index, reg in df.iterrows():   
+            for index, reg in df.iterrows():
                 # get 'not disjoint' countries
                 df_local = df[~df.geometry.disjoint(reg.geometry)]
                 neighbors = df_local[self.domain_gran].tolist()
-                
+
                 # remove own name from the list
                 neighbors = [ name for name in neighbors if reg[self.domain_gran] != name ]
-                
+
                 # add names of neighbors as NEIGHBORS value
                 df.at[index, "NEIGHBORS"] = ",".join(neighbors)
-                
+
             #Save modified shape file for future use
             print("Done. Saving domain shape structure for future use")
-            df.to_file(self.domain_shape_file_mdyn)   
+            df.to_file(self.domain_shape_file_mdyn)
 
         return df
 
     def load_subdomains(self):
-        
+
         #Get map subdomains shapes files
         self.subdomains_shape_file = self.subdomains_shape+".shp"
         self.subdomains_shape_file_mdyn = self.subdomains_shape+"mdyn.shp"
@@ -124,13 +124,13 @@ class Network:
             df = gpd.read_file(self.subdomains_shape_file_mdyn)
             print("  Subdomains shape file loaded.")
 
-        else: #Build map subdomains structure 
+        else: #Build map subdomains structure
             df = gpd.read_file(self.subdomains_shape_file)
             print("  Creating basic subdomains shape file...", end = '')
-            
+
             df["lonc"] = None #long centroid
             df["latc"] = None #lat centroid
-            for index, mun in df.iterrows():   
+            for index, mun in df.iterrows():
                 df.at[index, "lonc"] = mun.geometry.centroid.x
                 df.at[index, "latc"] = mun.geometry.centroid.y
 
@@ -146,7 +146,7 @@ class Network:
 
         #Indexes columns
         idx = np.arange(len(df))
-        df["idx"]=idx.astype(int)    
+        df["idx"]=idx.astype(int)
 
         #print(df)
         #print(self.regions_in, self.regions_in_latlon)
@@ -159,16 +159,16 @@ class Network:
 
         self.regions_in = self.df_subdomains[self.subdomains_gran].to_dict()
         filt = [self.subdomains_gran, "latc", "lonc", "idx"]
-        self.regions_in_latlon = self.df_subdomains.filter(filt).set_index(self.subdomains_gran).T.to_dict('list') 
+        self.regions_in_latlon = self.df_subdomains.filter(filt).set_index(self.subdomains_gran).T.to_dict('list')
         self.nreg_in = len(self.regions_in)
 
         #Outer regions (domain)
         #-------------------------
-        
+
         df_domain_local = self.df_domain[self.df_domain[self.domain_gran] == self.domain]
         self.domain_geometry=df_domain_local.geometry.values[0]
         self.domain_neib = df_domain_local.NEIGHBORS.values[0]
-        self.domain_neib = self.domain_neib.split(',') 
+        self.domain_neib = self.domain_neib.split(',')
         self.df_domain_nb=self.df_domain[self.df_domain[self.domain_gran].isin(self.domain_neib)]
         #print(self.df_domain_nb)
         self.df_domain_nb["idx"] = range(self.nreg_in, self.nreg_in+len(self.df_domain_nb), 1)
@@ -191,7 +191,7 @@ class Network:
         self.maxlons = np.round(self.maxlons, 1)
         self.minlats = np.round(self.minlats, 1)
         self.maxlats = np.round(self.maxlats, 1)
-        
+
         print("  Domain Box: ")
         print( "   Lon:", self.minlons, self.maxlons)
         print( "   Lat:", self.minlats, self.maxlats)
@@ -203,12 +203,12 @@ class Network:
         self.nlat = int((self.maxlats - (self.minlats))/self.dlat)
 
         #These are the datapoints
-        self.lon_bins_c = np.linspace(self.minlons, self.maxlons, self.nlon+1, endpoint=True) 
-        self.lat_bins_c = np.linspace(self.minlats, self.maxlats, self.nlat+1, endpoint=True) 
+        self.lon_bins_c = np.linspace(self.minlons, self.maxlons, self.nlon+1, endpoint=True)
+        self.lat_bins_c = np.linspace(self.minlats, self.maxlats, self.nlat+1, endpoint=True)
 
         #These are bins for datagrid (larger than datagrid) - Extended grid
-        self.lon_bins_ext = np.linspace(self.minlons-self.dlon/2, self.maxlons+self.dlon/2, self.nlon+2, endpoint=True) 
-        self.lat_bins_ext = np.linspace(self.minlats-self.dlat/2, self.maxlats+self.dlat/2, self.nlat+2, endpoint=True) 
+        self.lon_bins_ext = np.linspace(self.minlons-self.dlon/2, self.maxlons+self.dlon/2, self.nlon+2, endpoint=True)
+        self.lat_bins_ext = np.linspace(self.minlats-self.dlat/2, self.maxlats+self.dlat/2, self.nlat+2, endpoint=True)
 
         #2d grids
         self.lon_bins_c_2d, self.lat_bins_c_2d = np.meshgrid(self.lon_bins_c, self.lat_bins_c)
@@ -221,7 +221,7 @@ class Network:
         print("   nLat:", self.nlat)
 
         #Regions out are out of main domain
-        regions_out = self.df_domain_nb[self.domain_gran].to_dict() 
+        regions_out = self.df_domain_nb[self.domain_gran].to_dict()
         reg_out = list(regions_out.values())
         self.nreg_out = len(reg_out)
 
@@ -232,7 +232,7 @@ class Network:
         #Join in and out regions
         self.regions = {**self.regions_in, **self.regions_out}
         self.nregions = len(self.regions)
-        
+
         #Add extra region for all the rest (ocean or other places)
         #self.regions[str(self.nregions)] = 'NAN'
         #self.nregions = self.nregions + 1
@@ -243,13 +243,13 @@ class Network:
             print("  Last 10 regions:", dict(list(self.regions.items())[self.nregions-10:self.nregions]))
         else:
             print(self.regions)
-        
+
 
     #Build regions grid
     def build_grid_network(self):
-        
+
         print("Building grid network...")
-        
+
         self.gridname = 'maps/regions_'+self.domain+"_"+\
             self.subdomains+\
             "_lats"+str(self.minlats)+"_"+str(self.maxlats)+\
@@ -288,7 +288,7 @@ class Network:
                                         ireg=reg4
                                     else:
                                         ireg = -1
-                        
+
                         self.region_grid[i,j] = ireg
 
             #Save this region grid for furute use, as it takes time to build
@@ -299,9 +299,9 @@ class Network:
             #Map the regions
             map = Map(self)
             map.map_reg_data(self, self.gridname )
-            
+
     def get_closest_region(self, lat, lon):
-                
+
         p = Point(lon, lat)
         inmaindomain = p.within(self.domain_geometry)
         #print(p, inmaindomain)
@@ -312,10 +312,10 @@ class Network:
         #        print(p, debug)
         ireg=-1
         if inmaindomain:
-            
+
             lat_tmp=self.df_subdomains["latc"].to_numpy()
             lon_tmp=self.df_subdomains["lonc"].to_numpy()
-            
+
             latv = np.full(len(lat_tmp), lat)
             lonv = np.full(len(lon_tmp), lon)
 
@@ -323,10 +323,10 @@ class Network:
 
             k = 10 # k nearest neighbours
             inearreg = np.argpartition(dist_tmp, k) #[0:k]
-            
+
             #Use geometry to check if point in subregion
             for i in inearreg:
-                
+
                 subreg_geometry = self.df_subdomains.loc[i, "geometry"]
                 in_region=p.within(subreg_geometry)
                 #if debug:
@@ -334,13 +334,13 @@ class Network:
                 if in_region:
                     ireg = i
                     return ireg
-        
+
         else:
             #check if in neighbour states
-           
-            for index, nb in self.df_domain_nb.iterrows(): 
-                
-                #nb_name=nb[self.domain_gran]                
+
+            for index, nb in self.df_domain_nb.iterrows():
+
+                #nb_name=nb[self.domain_gran]
                 innbreg=p.within(nb.geometry)
                 #if debug:
                 #    print(index, nb, innbreg)
@@ -352,7 +352,7 @@ class Network:
         return ireg
 
     def add_reg_to_df(self, dom, data):
-        
+
         for day in data:
             #Add column with region tag
             #for each event
@@ -380,7 +380,7 @@ class Network:
             #day.df.to_csv("tmp.csv")
 
     def add_reg_to_daydf(self, daydata):
-        
+
         #Add column with region tag
         #for each event
         for i in range(2):
@@ -391,18 +391,18 @@ class Network:
             #Remova data that is outside of the box
             lon=np.where((lon>self.maxlons)|(lon<self.minlons), np.nan, lon)
             lat=np.where((lat>self.maxlats)|(lat<self.minlats), np.nan, lat)
-            
+
             lonnan=np.isnan(lon)
             latnan=np.isnan(lat)
             nan = lonnan | latnan
-            
+
             ilon=((lon[~nan]-self.minlons)/self.dlon).astype(int)
             ilat=((lat[~nan]-self.minlats)/self.dlat).astype(int)
 
             reg = np.zeros(daydata.n).astype(int)
             reg[nan] = -1
             reg[~nan]=self.region_grid[ilat, ilon]
-            
+
             daydata.df['reg'+s]=reg
 
         #Add column with moved or not
@@ -414,15 +414,15 @@ class Network:
         #day.df.to_csv("tmp.csv")
 
     def calc_transition_matrix(self, day):
-        
+
         print()
         print("Generating transition matrices...", end="")
-                
+
         df=day.df
         #df.to_csv("tmp.csv", header=True)
         table = df.pivot_table(values='dt1', index=['reg1'],\
                      columns=['reg0'], aggfunc=np.count_nonzero, fill_value=0, dropna=False)
-        
+
         #print(table.columns)
         #Remove other outer regions from transition matrix
         #consider movements only data inside the domain
@@ -445,7 +445,7 @@ class Network:
         except:
             pass
 
-        
+
 
         #debug tool only!!!!
         #table = table.drop(columns=[5])
@@ -468,7 +468,7 @@ class Network:
         nreg=len(self.regions)
         reg = list(self.regions.keys())
         n , m = mat.shape
-        
+
         if nreg0==nreg:
             print("Matrix check origin: ok!...", end="")
         elif nreg>nreg0:
@@ -476,7 +476,7 @@ class Network:
             missing_col=list(set(reg)-set(reg0))
             missing_col.sort()
             print(missing_col, end="")
-            
+
             for i in missing_col:
                 col = np.zeros((n,1))
                 if i in reg1:
@@ -523,7 +523,7 @@ class Network:
             print()
 
         #np.fill_diagonal(mat, mat.diagonal() + steady_users_per_reg)
-        #print(mat)        
+        #print(mat)
         if self.nregions < 10:
             print("Transition matrix including steady users")
             matprint(mat)
@@ -534,7 +534,7 @@ class Network:
         if self.nregions<10:
             print("Normalized transition matrix (transition probability)")
             matprint(mat_normed)
-        
+
         if self.nregions > 10:
             print("..done")
 
@@ -546,7 +546,7 @@ class Network:
         filename = self.subdomains_pop # "maps/population/population_sp_mun.csv"
         subdomains_pop_labels = self.subdomains_pop_labels # ["municipio", "populacao_estimada"]
 
-        df_pop = pd.read_csv(filename, sep = ";") 
+        df_pop = pd.read_csv(filename, sep = ";")
         df_pop[subdomains_pop_labels[0]]=df_pop[subdomains_pop_labels[0]].str.upper()
 
         self.reg_pop = np.zeros([self.nregions])
@@ -560,12 +560,12 @@ class Network:
                 sys.exit(1)
             self.reg_pop[reg] = pop
 
-        
+
         #Population - outer regions
         filename = self.domain_pop # "maps/population/population_sp_mun.csv"
         domain_pop_labels = self.domain_pop_labels # ["municipio", "populacao_estimada"]
 
-        df_pop = pd.read_csv(filename, sep = ";") 
+        df_pop = pd.read_csv(filename, sep = ";")
         df_pop[domain_pop_labels[0]]=df_pop[domain_pop_labels[0]].str.upper()
         for reg in range(self.nreg_out):
             i=self.nreg_in+reg
@@ -577,4 +577,3 @@ class Network:
                 sys.exit(1)
             self.reg_pop[reg] = pop
         print("Done.")
-        
