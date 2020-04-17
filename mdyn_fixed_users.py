65b9f86992fe49e374e9988f9a257744229237a8
Pedro Peixoto <ppeixoto@usp.br>
2020-04-17 19:36:32 -0300

    new isolation index analysis

--- mdyn_fixed_users.py
+++ mdyn_fixed_users.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-
 import statistics
 import numpy as np
 import pandas as pd
@@ -23,7 +22,150 @@ from datetime import timedelta
 from mdyn_map import Map
 import mdyn_extras as mex
 
-class fixed_users():
+class fixed_users_by_date():
+    def __init__(self, network, ipar):
+        
+        data_dir = ipar.data_dir
+        data_format = ipar.data_format
+        date_ini = ipar.date_ini
+        date_end = ipar.date_end
+        dump_dir = ipar.dump_dir
+        load = ipar.load_data
+        
+        print("")
+        print(" Isolation index Data Analysis")
+        print("-----------------------------")
+        if not os.path.exists(data_dir):
+            print("Directory doesnt exist. Did you type it wrong? ", data_dir)
+            sys.exit(1)
+
+        #Ensure we have the "/"
+        if data_dir[-1]!="/":
+            self.data_dir = data_dir+"/"
+        else:
+            self.data_dir=data_dir
+
+        self.date_ini = date_ini
+        if date_end == None:
+            self.date_end = date_ini
+        else:
+            self.date_end = date_end
+
+        self.date_ini_obj = datetime.strptime(self.date_ini, '%Y-%m-%d')
+        self.date_end_obj = datetime.strptime(self.date_end, '%Y-%m-%d')
+        self.days = (self.date_end_obj - self.date_ini_obj).days + 1
+        
+        print("Folder with data:", self.data_dir)
+        print("Initial date:", self.date_ini)
+        print("Final date:", self.date_end)
+        print("Number of days to analyse:", self.days)
+
+        self.load = load
+        self.data_format = data_format
+        self.dump_dir = dump_dir
+        
+        if not os.path.exists(self.dump_dir):
+            os.makedirs(self.dump_dir)
+            
+        print("Output folder:", self.dump_dir)
+
+        self.base_name = network.domain+"_"+network.subdomains
+
+        self.proc_data(network)
+
+    def proc_data(self, network):
+
+        #Data is separated by date
+        self.days_all = [] #List of dates per day
+        self.dates_dirs = [] #directory of day data
+        self.dfs = []
+        print()
+        #Loop over folders with days 
+        for day in mex.daterange(self.date_ini_obj, self.date_end_obj+timedelta(days=1)):
+            print("Processing day: ", day)
+
+            self.days_all.append(day)
+
+            self.dates_dirs.append(self.data_dir+"day="+day.strftime("%Y-%m-%d")+"/")
+            sday=day.strftime("%Y-%m-%d")
+            if self.data_format == "Parquet":
+                local_dir = self.data_dir+"day="+sday+"/"
+                df = mex.read_pq2df(local_dir, "original", True)
+                df = self.org_data(df, network, local_dir)
+                df['day']=sday
+                print(df)
+                self.dfs.append(df)
+            else:
+                print("Invalid file format")
+
+            print()
+
+        df_all = pd.concat(self.dfs)
+        outfile = self.dump_dir+network.domain+"_"+network.subdomains+"_"+ \
+            self.date_ini+" "+self.date_end+"_iso_index.csv"
+        df_all.to_csv(outfile) 
+        print(df_all)
+
+
+            
+    def org_data(self, dflocal, network, data_dir):
+            #Organize data into nice dataframe
+
+            n=len(dflocal)
+            
+            pklfile = data_dir+network.domain+"_"+network.subdomains+"_data_proc.pkl"
+            csvfile = data_dir+network.domain+"_"+network.subdomains+"_data_proc.csv"
+
+            if not os.path.exists(pklfile):
+                #Convert lat lon from dict to numpy array
+                lat = dflocal['lat0']
+                lon = dflocal['lng0']
+
+                #Remove data that is outside of the box
+                lon=np.where((lon>network.maxlons)|(lon<network.minlons), np.nan, lon)
+                lat=np.where((lat>network.maxlats)|(lat<network.minlats), np.nan, lat)
+
+                lonnan=np.isnan(lon)
+                latnan=np.isnan(lat)
+                nan = lonnan | latnan
+
+                ilon=((lon[~nan]-network.minlons)/network.dlon).astype(int)
+                ilat=((lat[~nan]-network.minlats)/network.dlat).astype(int)
+
+                dflocal['lat']=lat
+                dflocal['lon']=lon
+
+                dflocal.drop('lat0', axis=1, inplace=True)
+                dflocal.drop('lng0', axis=1, inplace=True)
+
+                reg = np.zeros(n).astype(int)
+                reg[nan] = -1
+                reg[~nan]=network.region_grid[ilat, ilon]
+                dflocal['reg']=reg
+                
+                #Drop place out of region of interest
+                dflocal.dropna(subset = ["lat"], inplace=True)
+                dflocal.dropna(subset = ["lon"], inplace=True)
+                
+                dflocal['reg_name']=dflocal['reg']
+                dflocal['reg_name']=dflocal['reg_name'].map(network.regions).fillna(dflocal['reg_name'])
+                
+                dflocal = dflocal.groupby('reg_name', as_index=False).agg({'lat':'mean', 'lon':'mean',
+                    'reg':'mean', 'left_home':'sum', 'active_users_in_month':'sum'})
+                
+                dflocal['move'] = dflocal['left_home']/dflocal['active_users_in_month']
+                dflocal['iso'] = 1.0 - dflocal['move']
+                print(dflocal)
+                print("Saving pre-processed data-file for future use:", pklfile, csvfile)
+                dflocal.to_pickle(pklfile) 
+                dflocal.to_csv(csvfile) 
+            else:
+                dflocal = pd.read_pickle(pklfile)
+                print("Loading pre-processed data-file:", pklfile)
+
+            return dflocal
+
+class fixed_users_general():
     def __init__(self, network, ipar):
         
         data_dir = ipar.data_dir 
