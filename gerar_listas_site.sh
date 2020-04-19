2633ea78f4deca6e34673c5ba2ca45d7f29f8fb7
Pedro Peixoto <ppeixoto@usp.br>
2020-04-19 15:39:19 -0300

    site

--- gerar_listas_site.sh
+++ gerar_listas_site.sh
@@ -3,6 +3,7 @@
 echo "Processando Relatorios"
 echo "----------------------"
 
+work="workspace"
 fig_dir="Figuras"
 relat_dir="Relatorios"
 main_relat_dir="Relatórios"
@@ -12,8 +13,9 @@ html_s2=" target="\""_blank"\"" rel="\""noopener"\"">"
 html_s3="</a></li>"
 html_s4="</a>"
 long_name="Relatório sobre isolamento durante a pandemia.pdf"
+texto="Cidades em que não há informações com confiança suficiente para calcular o Índice de Isolamento Social:"
 
-for dir in "Workspace"/*    
+for dir in "${work}"/*    
 do
     dir=${dir%*/}
     name=${dir##*/}
@@ -25,6 +27,8 @@ do
     site=$base"/site.txt"
     echo $site
     echo "" > "${site}"
+
+    #entrar no diretorio de relatorios
     cd $relat_dir
 
     path1=$name/$relat_dir
@@ -36,7 +40,7 @@ do
     
     main_label=${long_name%".pdf"}
     echo $html_s0$path1/$name"_"$long_name"\""$html_s2 $main_label "-" $name $html_s4 >> "${site}"
-    
+
     echo "" >>  "${site}"
     echo "<strong>Índice nos Municípios</strong>:" >> "${site}"
     echo "<ul>" >> "${site}"
@@ -44,16 +48,39 @@ do
     #Lista de municipios
     #file pattern name
     for relat in $name*.pdf; do
-	label="${relat#$name}"
-	label=${label%"$long_name"}
-	label=`echo $label | sed -r 's/_/ - /g'`
-	length=${#label}
-	if [ ${#label} -ge 4 ]; then
-	    label=`echo $label | sed -r 's/-/ /g'`
-	    echo $label
-	    echo $html_s1$path1/${relat}"\""$html_s2 $label $html_s3 >> "${site}"
-	fi
+        label="${relat#$name}"
+        label=${label%"$long_name"}
+        label=`echo $label | sed -r 's/_/ - /g'`
+        length=${#label}
+        if [ ${#label} -ge 4 ]; then
+            label=`echo $label | sed -r 's/-/ /g'`
+            echo $label
+            echo $html_s1$path1/${relat}"\""$html_s2 $label $html_s3 >> "${site}"
+        fi
     done
+    echo "</ul>" >> "${site}"
+
+    #sai do diretorio de relatorios
+    cd ..
+    ls
+    #municipios sem dados
+    csvfile=$name"_cidades_sem_relatorio.csv"
+    #cat $csvfile
+    OLDIFS=$IFS
+    IFS=','
+    echo "" >> "${site}"
+    echo $texto >> "${site}"
     echo "<ul>" >> "${site}"
+    #[ ! -f $csvfile ] && { echo "$csvfile file not found"; exit 99; }
+    while read id mun
+    do
+        if [ "$mun" != "\""problemas"\"" ]; then
+            mun_s=`echo $mun | sed -r 's/"//g'`
+            echo "<li>" "$mun_s" "</li>" >> "${site}"
+        fi
+    done <  "${csvfile}"
+    IFS=$OLDIFS
+    echo "</ul>" >> "${site}"
+
     exit 1
 done
---
+++ limpa_relatorios.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Limpando Relatorios"
+echo "----------------------"
+
+fig_dir="Figuras"
+relat_dir="Relatorios"
+main_relat_dir="Relatórios"
+base=`pwd`
+
+for dir in "Workspace"/* 
+do
+    dir=${dir%*/}   
+    echo ${dir##*/}
+
+    #Entrar no diretorio do estado
+    cd $dir
+    
+    #Entre no diretorio de relatorios
+    cd "${relat_dir}"
+    echo `pwd`
+
+    #Limpar
+    rf -rf "${fig_dir}" #$bkp/.
+    rm -f *.fls
+    rm -f *.tex #$bkp/.
+    rm -f *.aux
+    rm -f *.log
+    rm -f *.fdb_latexmk
+
+    #Sair do diretorio de relatorios e estado
+    cd "${base}"
+    echo `pwd`
+done
