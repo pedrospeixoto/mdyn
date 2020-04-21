a48e3a7fd7cd782bc83be4f8554d0cfc122a4457
Pedro Peixoto <ppeixoto@usp.br>
2020-04-21 17:38:22 -0300

    run on main dir

--- pysrc/mdyn_shpfile_check.py
+++ pysrc/mdyn_shpfile_check.py
@@ -1,6 +1,10 @@
-import shapefile
+import pandas as pd
+import geopandas as gpd
 import sys
 
-shape = shapefile.Reader(sys.argv[1])
+df = gpd.read_file(sys.argv[1])
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(df)
+print(df.columns)
+print(df.head())
 
-print(shape)
