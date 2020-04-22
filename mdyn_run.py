96636df431df9feb4e60b39d10cb5353ff88be5c
Martin Schreiber <schreiberx@gmail.com>
2020-04-22 22:59:25 +0200

    added parallelized version

--- mdyn_run.py
+++ mdyn_run.py
@@ -2,6 +2,7 @@
 # conda enviroment
 # conda activate mdyn
 
+import time
 import sys
 import os
 import warnings
@@ -19,6 +20,8 @@ import mdynpy.mdyn_isol_index as isol
 import mdynpy.mdyn_fixed_users as fx
 import mdynpy.mdyn_seir as seir
 
+
+
 #Input parameters
 #-----------------------------
 ipar, run_opt = mex.get_input(sys.argv)
@@ -26,38 +29,70 @@ ipar, run_opt = mex.get_input(sys.argv)
 
 ipar.load_network = True
 
+
+
 #Initialize network
 #-----------------------------
+time_start = time.time()
 network = Network(ipar)
+time_end = time.time()
+print("Execution time "+str(time_end-time_start)+" seconds")
         
 #Initialize Data
 #-----------------------------
 if run_opt < 30:
+    time_start = time.time()
     mdyn = MobileDynamics(ipar) 
-
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
 
 #Build model = generate movement model
 if run_opt == 0:
+    time_start = time.time()
     mdyn.build_model(network)
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
+
 
 if run_opt == 1:
+    time_start = time.time()
     mmat.analyse_move_mats(mdyn, network, ipar)
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
 
 if run_opt == 2:
+    time_start = time.time()
     mmat.analyse_move_mats_dow(mdyn, network, ipar)
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
 
 if run_opt == 20:
+    time_start = time.time()
     mmat.simulate_move_mats(mdyn, network, ipar)
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
 
 if run_opt == 21:
+    time_start = time.time()
     mmat.simulate_model(mdyn, network, ipar)
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
 
 if run_opt == 22:
+    time_start = time.time()
     seir.simulate_seir_model(mdyn, network, ipar)
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
 
 if run_opt == 30:
+    time_start = time.time()
     isol.isol_index(network, ipar)
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
 
 if run_opt == 31:
+    time_start = time.time()
     fx.fixed_users_by_date(network, ipar)
+    time_end = time.time()
+    print("Execution time "+str(time_end-time_start)+" seconds")
     
