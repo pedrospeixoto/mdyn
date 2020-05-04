e058e7c37d39d1229be3cfe23bd98714fde627a4
Pedro Peixoto <ppeixoto@usp.br>
2020-05-03 21:48:44 -0300

    Nordeste

--- mdynpy/mdyn_extras.py
+++ mdynpy/mdyn_extras.py
@@ -361,6 +361,35 @@ state_abrv2name = {
     'TO': 'Tocantins'
 }
 
+state_reg = {'Acre': 'Norte', 
+    'Alagoas': 'Nordeste', 
+    'Amapá': 'Norte', 
+    'Amazonas': 'Norte', 
+    'Bahia': 'Nordeste', 
+    'Ceará': 'Nordeste', 
+    'Distrito Federal': 'Centro-Oeste', 
+    'Espirito Santo': 'Sudeste', 
+    'Goiás': 'Centro-Oeste', 
+    'Maranhão': 'Nordeste', 
+    'Mato Grosso': 'Centro-Oeste', 
+    'Mato Grosso do Sul': 'Centro-Oeste', 
+    'Minas Gerais': 'Sudeste', 
+    'Pará': 'Norte', 
+    'Paraíba': 'Nordeste', 
+    'Paraná': 'Sul', 
+    'Pernambuco': 'Nordeste', 
+    'Piauí': 'Nordeste', 
+    'Rio de Janeiro': 'Sudeste', 
+    'Rio Grande do Norte': 'Nordeste', 
+    'Rio Grande do Sul': 'Sul', 
+    'Rondônia': 'Norte', 
+    'Roraima': 'Norte', 
+    'Santa Catarina': 'Sul', 
+    'São Paulo': 'Sudeste', 
+    'Sergipe': 'Nordeste', 
+    'Tocantins': 'Norte'}
+
+    
 #Ploting helpers
 markers = [ 'o', 'v', '^', '<', '>', 's', 'p', '*', 'h', 'H', '+', 'x', 'D', '.', ',', 'o', 'v', '^', '<', '>', '1', '2', '3', '4', '8', 's', 'p', '*', 'h', 'H', '+', 'x', 'D']
 thinmarkers = ['.', '1', '2', '3', '4', '8']
---
+++ mdynpy/mdyn_shpfile_merge.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import sys
+
+df = gpd.read_file(sys.argv[1])#Shape file with br_municipios
+#with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+#    print(df)
+
+print(df)
+df.CD_GEOCMU=df.CD_GEOCMU.astype(int)
+
+df2 = pd.read_csv(sys.argv[2]) #uf_vs_municipio
+df2.CD_GEOCMU=df2.CD_GEOCMU.astype(int)
+
+df=df.sort_values(by=['CD_GEOCMU'])
+#df=df.set_index('CD_GEOCMU')
+print(df)
+df2=df2.sort_values(by=['CD_GEOCMU'])
+#df2=df2.set_index('CD_GEOCMU')
+print(df2)
+
+
+df3 = pd.merge(df2, df, on='CD_GEOCMU')
+
+df3.to_csv("maps/br_municipios/br_mun_with_uf.csv")
+print("done csv")
+
+df3 = gpd.GeoDataFrame(df3, geometry='geometry')
+print(df3)
+df3.to_file('maps/br_municipios/br_mun_with_uf.shp', driver='ESRI Shapefile')
+print("done shp")
+
+
+
