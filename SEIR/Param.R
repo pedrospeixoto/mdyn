bbcb0cec2f465b9dd960e37462d6237d5a46603b
dmarcondes <diegormmarcondes@gmail.com>
2020-06-02 13:13:22 -0300

    SEIR

--- SEIR/Param.R
+++ SEIR/Param.R
@@ -73,7 +73,7 @@ par$s <- c(0.01,0.5,1,1.5,2,2.5,3)
 # #n_dependent <- as.numeric((length(cand_beta)^10)^4)
 # n_models <- n_independent
 # sample_size <- mc(p = 0.1*n_models,c = 0.1,delta = 0.99,cardinality = n_models)
-sample_size <- 1000
+sample_size <- 10000
 max_models <- 100000
 
 source("mdyn/SEIR/SEIR_COVID19.R",sep = "")
