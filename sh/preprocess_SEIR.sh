84962bc6399e5490a2f6fffae36ffae7dbdc719c
dmarcondes <diegormmarcondes@gmail.com>
2020-06-04 17:16:20 -0300

    sh

--- sh/preprocess_SEIR.sh
+++ sh/preprocess_SEIR.sh
@@ -29,15 +29,22 @@ cd /storage/SEIR/$1/Videos
 for d in *     # list directories
 do
   #Convert files in casos
-  mogrify -density 100 -format png ./$d/casos/*.pdf;
+  mogrify -density 100 -format png ./$d/casos/*.pdf &
+  #Convert files in mortes
+  mogrify -density 100 -format png ./$d/mortes/*.pdf &
+done;
+
+for d in *     # list directories
+do
+  #Convert files in casos
   find ./$d/casos/ -maxdepth 1 -type f -iname "*.pdf" -delete;
-  ffmpeg -framerate 1 -i "./$d/casos/%03d".png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p /storage/SEIR/$1/casos_$d.mp4 &
+  ffmpeg -framerate 5 -i "./$d/casos/%03d".png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p /storage/SEIR/$1/casos_$d.mp4 &
 
   #Convert files in mortes
-  mogrify -density 100 -format png ./$d/mortes/*.pdf;
   find ./$d/mortes/ -maxdepth 1 -type f -iname "*.pdf" -delete;
-  ffmpeg -framerate 3 -i "./$d/mortes/%03d".png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p /storage/SEIR/$1/mortes_$d.mp4 &
-done
+  ffmpeg -framerate 5 -i "./$d/mortes/%03d".png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p /storage/SEIR/$1/mortes_$d.mp4 &
+done;
 
 #Copy video files to ShinyApp
+cd /storage/SEIR/$1
 find . -maxdepth 1 -type f -iname "*.mp4" -exec cp {} /storage/ShinyApps/seircovid19/www/ \;
