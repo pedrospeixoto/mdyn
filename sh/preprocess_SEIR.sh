077f69dde03a4dbd059d56cbc869c01e1517587a
dmarcondes <diegormmarcondes@gmail.com>
2020-06-04 15:34:30 -0300

    sh

--- sh/preprocess_SEIR.sh
+++ sh/preprocess_SEIR.sh
@@ -16,7 +16,7 @@ find . -maxdepth 1 -type f -iname "*.png" -exec cp {} /storage/ShinyApps/seircov
 
 #Convert files in EPCurve
 mogrify -density 100 -format png ./EPCurve/*.pdf;
-find .EPCurve/ -maxdepth 1 -type f -iname "*.pdf" -delete;
+find ./EPCurve/ -maxdepth 1 -type f -iname "*.pdf" -delete;
 find . -maxdepth 1 -type f -iname "*.png" -exec cp {} /storage/ShinyApps/seircovid19/www/ \; &
 
 #Convert files in validate
@@ -28,13 +28,13 @@ find . -maxdepth 1 -type f -iname "*.png" -exec cp {} /storage/ShinyApps/seircov
 for d in "Videos"/*     # list directories in the form "/tmp/dirname/"
 do
   #Convert files in casos
-  mogrify -density 100 -format png ./Videos/$d/casos/*.pdf;
-  find ./Videos/$d/casos/ -maxdepth 1 -type f -iname "*.pdf" -delete;
+  mogrify -density 100 -format png ./$d/casos/*.pdf;
+  find ./$d/casos/ -maxdepth 1 -type f -iname "*.pdf" -delete;
   ffmpeg -framerate 1 -i "%03d".png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p /storage/SEIR/$1/casos_$d_$1.mp4 &
 
   #Convert files in mortes
-  mogrify -density 100 -format png ./Videos/$d/mortes/*.pdf;
-  find ./Videos/$d/mortes/ -maxdepth 1 -type f -iname "*.pdf" -delete;
+  mogrify -density 100 -format png ./$d/mortes/*.pdf;
+  find ./$d/mortes/ -maxdepth 1 -type f -iname "*.pdf" -delete;
   ffmpeg -framerate 1 -i "%03d".png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p /storage/SEIR/$1/mortes_$d_$1.mp4 &
 done
 
