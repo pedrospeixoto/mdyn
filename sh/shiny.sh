4a75e39cacac946d8a1c22ce0272b22bfe654050
dmarcondes <diegormmarcondes@gmail.com>
2020-06-04 13:45:44 -0300

    Shiny

--- sh/shiny.sh
+++ sh/shiny.sh
@@ -2,3 +2,4 @@
 #update Shiny apps
 
 rsync -v --stats --progress -avz -e "ssh -p 2223" /home/dmarcondes/mdyn/ShinyApps dmarcondes@shiny.ime.usp.br:
+rsync -v --stats --progress -avz -e "ssh -p 2223" dmarcondes@shiny.ime.usp.br: /home/dmarcondes/mdyn/ShinyApps
