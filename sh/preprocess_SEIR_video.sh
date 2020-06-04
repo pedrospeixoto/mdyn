675b5eb2760532dbfff2f359d80ef5a4a4776a49
dmarcondes <diegormmarcondes@gmail.com>
2020-06-04 20:10:48 -0300

    sh

--- sh/preprocess_SEIR_video.sh
+++ sh/preprocess_SEIR_video.sh
@@ -17,3 +17,6 @@ done;
 #Copy video files to ShinyApp
 cd /storage/SEIR/$1
 find . -maxdepth 1 -type f -iname "*.mp4" -exec cp {} /storage/ShinyApps/seircovid19/www/ \;
+cd /storage/SEIR/$1/Videos/Estado
+cp casos/ /storage/ShinyApps/seircovid19/www/casos/ \;
+cp mortes/ /storage/ShinyApps/seircovid19/www/mortes/ \;
