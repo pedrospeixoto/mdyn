f673f669c39779607f0bbd8b230a50321c5d6537
dmarcondes <diegormmarcondes@gmail.com>
2020-06-12 13:07:46 -0300

    SEIR

--- SEIR/sample_parameters.R
+++ SEIR/sample_parameters.R
@@ -17,8 +17,8 @@ sample_parameters <- function(par,day_validate){
   parK$delta <- par$delta #delta
   parK$sites <- par$sites #Number of sites
   parK$s <- sample(x = par$s,size = 1) #s
-  #parK$lift <- par$lift
-  parK$upI <- (1-parK$pS)/parK$pS #Number of missed cases for each one in statistics
+  parK$lift <- par$lift
+  parK$upI <- par$lift*(1-parK$pS)/parK$pS #Number of missed cases for each one in statistics
   
   #Parameters
   parK$gammaI <- 1/parK$Te #Rate from Exposed to Infected
