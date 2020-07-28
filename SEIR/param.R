752e48fddd58f0d6aa8642a78b4f960358fc5073
dmarcondes <diegormmarcondes@gmail.com>
2020-07-28 13:14:37 -0300

    SEIR

--- SEIR/param.R
+++ SEIR/param.R
@@ -13,8 +13,8 @@ seed <- as.numeric(Sys.Date()) #Seed
 par <- list() #Candidate values of model parameters
 d_max <- Sys.Date() #"2020-06-14"
 simulate_length <- as.numeric(ymd("2020-12-31") + 5 - ymd(d_max)) #Number of days to simulate
-error_I <- 0.059
-error_D <- 0.0575
+error_I <- 0.06
+error_D <- 0.06
 
 #Set mobility matrix
 par$mob <- list()
