545f32de9e708339ad272ca24804713f2da1e096
dmarcondes <diegormmarcondes@gmail.com>
2020-07-30 15:38:24 -0300

    SEIR

--- SEIR/lift_death_research.R
+++ SEIR/lift_death_research.R
@@ -1,11 +1,11 @@
 #Calculate lift of the death rate
 lift_death <- function(obs,end_validate,par){
-  tmp <- obs %>% filter(date >= ymd(end_validate) - 14) #Only end_validate
+  tmp <- obs %>% filter(date >= ymd(end_validate) - 30) #Only end_validate
   lift <- data.frame("city" = NA,"lift" = NA)
   for(c in unique(tmp$city)){
     tmp1 <- tmp %>% filter(city == c)
-    deaths <- tmp1$deaths_corrected[tmp1$date == ymd(end_validate)] - tmp1$deaths_corrected[tmp1$date == ymd(end_validate) - 14]
-    cases <- tmp1$confirmed_corrected[tmp1$date == ymd(end_validate)] - tmp1$confirmed_corrected[tmp1$date == ymd(end_validate) - 14]
+    deaths <- tmp1$deaths_corrected[tmp1$date == ymd(end_validate)] - tmp1$deaths_corrected[tmp1$date == ymd(end_validate) - 30]
+    cases <- tmp1$confirmed_corrected[tmp1$date == ymd(end_validate)] - tmp1$confirmed_corrected[tmp1$date == ymd(end_validate) - 30]
     lift <- rbind.data.frame(lift,data.frame("city" = c,"lift" = deaths/cases))
   }
   rate <- sum(tmp$new_death_cor)/sum(tmp$new_infected_cor) #death rate state
@@ -14,7 +14,7 @@ lift_death <- function(obs,end_validate,par){
   lift$lift[is.na(lift$lift)] <- 1 #fill NA with 1
   lift$lift[lift$lift == 0] <- 1
   #lift$lift[lift$lift < 0.9] <- 1 #Truncate
-  lift$lift[lift$lift > 2] <- 2 #Truncate
+  lift$lift[lift$lift > 1.5] <- 1.5 #Truncate
   
   return(lift$lift)
 }
