32153b1ecd1d254ef036294f31e1b4ae324d85cb
dmarcondes <diegormmarcondes@gmail.com>
2020-07-30 15:08:55 -0300

    SEIR

--- SEIR/beta_research.R
+++ SEIR/beta_research.R
@@ -52,7 +52,7 @@ beta <- function(parK,t,lambda,drs,day,obs){
   c <- obs %>% filter(date == ymd(day) & confirmed_corrected > 500) 
   beta[par$names %in% c$city] <- b[par$names %in% c$city]
   
-  beta[beta <= 0] <- min(beta[beta > 0])
+  #beta[beta <= 0] <- min(beta[beta > 0])
   
   return(beta)
 }
