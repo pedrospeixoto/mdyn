61a490de443a553e0445d2d6060ed6b4e88a94b3
dmarcondes <diegormmarcondes@gmail.com>
2020-09-16 18:34:21 -0300

    SEIR

--- SEIR/seir_solver.R
+++ SEIR/seir_solver.R
@@ -5,6 +5,7 @@ solve_seir <- function(y,times,derivatives,parms){
     names(mod)[-1] <- names(y)
   for(t in times[-1]){
     y <- y + unlist(derivatives(t = t-1,Y = y,parK = parms))
+    y[y < 0] <- 0.5
     mod[t,] <- c(t,y)
   }
   return(mod)
