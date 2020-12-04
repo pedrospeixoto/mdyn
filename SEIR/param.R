546bd949bc2b2146989bda5ed6a16b8034c9fcf2
dmarcondes <diegormmarcondes@gmail.com>
2020-12-04 19:15:24 -0300

    SEIR

--- SEIR/param.R
+++ SEIR/param.R
@@ -12,7 +12,7 @@ pos <- Sys.Date() #"teste" #What to add at the end of all output files
 seed <- as.numeric(Sys.Date()) #Seed
 par <- list() #Candidate values of model parameters
 d_max <- Sys.Date() #"2020-06-14"
-simulate_length <- as.numeric(ymd("2020-12-31") + 5 - ymd(d_max)) #Number of days to simulate
+simulate_length <- as.numeric(ymd("2021-05-31") + 5 - ymd(d_max)) #Number of days to simulate
 error_I <- 0.02
 error_D <- 0.02
 
